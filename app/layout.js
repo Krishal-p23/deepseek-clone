292219fdb948c5d603dc71813df8cb5dfbf8b976
Krishal <aruhhh23@gmail.com>
2026-06-15 15:37:46 +0530

    Clerk setup

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import { Inter, Roboto } from "next/font/google";
 import "./globals.css";
 
@@ -18,11 +19,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${inter.className} ${roboto.className} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
+    <ClerkProvider>
+      <html
+        lang="en"
+        className={`${inter.className} ${roboto.className} h-full antialiased`}
+      >
+        <body className="min-h-full flex flex-col">
+
+          {children}
+
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
