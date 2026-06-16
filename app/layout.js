0e6dbcdd6f5feeec8e6ec9b59afa0e562843c3b2
Krishal <aruhhh23@gmail.com>
2026-06-17 04:43:27 +0530

    Add messages retrieval from backend with formatted frontend display

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter, Roboto } from "next/font/google";
 import "./globals.css";
+import "./prism.css";
 import { AppContextProvider } from "@/context/AppContext";
 import { Toaster } from "react-hot-toast";
 
--- components/PromptBox.jsx
+++ components/PromptBox.jsx
@@ -95,7 +95,7 @@ const PromptBox = ({ isLoading, setIsLoading }) => {
     };
 
     return (
-        <form className={`w-full ${false ? "max-w-3xl" : "max-w-2xl"}
+        <form className={`w-full ${selectedChat.messages.length > 0 ? "max-w-3xl" : "max-w-2xl"}
         bg-[#404045] p-4 rounded-3xl mt-4 transition-all`} onSubmit={sendPrompt}>
             <textarea
                 className="outline-none w-full resize-none overflow-hidden break-words bg-transparent"
